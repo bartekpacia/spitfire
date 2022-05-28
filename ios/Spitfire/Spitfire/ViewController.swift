877738e35485f3f8895ae6f9d784a13479874506
Bartek Pacia <barpac02@gmail.com>
2022-05-28 14:15:58 +0200

    create some xcode project

---
+++ ios/Spitfire/Spitfire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spitfire
+//
+//  Created by Bartek Pacia on 13/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
