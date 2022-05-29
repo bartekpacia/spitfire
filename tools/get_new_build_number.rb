89469251876d5c70969b144364cb8008846174ca
Bartek Pacia <barpac02@gmail.com>
2022-05-29 02:59:44 +0200

    add ruby tools

---
+++ tools/get_new_build_number.rb
@@ -0,0 +1,37 @@
+require_relative "./get_highest_build_number.rb"
+
+def get_new_build_number(
+    bundle_identifier:,
+    package_name:,
+    google_play_json_key_path:,
+    firebase_json_key_path:,
+    firebase_app_ios:,
+    firebase_app_android:
+  )
+  puts "pwd: #{Dir.pwd}"
+
+  file = "../../new_build_number.txt"
+  if(File.exist?(file)) 
+    puts "file exists - reading!" 
+    highest_build_number = File.read(file).to_i
+  else 
+    puts "file not found - getting highest build number"
+    highest_build_number = get_highest_build_number(
+      bundle_identifier: bundle_identifier,
+      package_name: package_name,
+      google_play_json_key_path: google_play_json_key_path,
+      firebase_json_key_path: firebase_json_key_path,
+      firebase_app_ios: firebase_app_ios,
+      firebase_app_android: firebase_app_android,
+    )
+    
+    File.open(file, "w") { |f| f.write "#{highest_build_number}\n" }
+  end
+
+  puts "highest_build_number: #{highest_build_number}"
+
+  new_highest_build_number = highest_build_number + 1
+  puts "new_highest_build_number: #{new_highest_build_number}"
+
+  return new_highest_build_number
+end
