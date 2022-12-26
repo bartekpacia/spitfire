c8b5f0f18a9ddcb6806d88b73816fd0e4facaed6
Bartek Pacia <barpac02@gmail.com>
2022-12-27 00:54:37 +0100

    migrate android/build.gradle to kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,126 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+val keystorePropertiesFile = rootProject.file("./android/keystore.properties")
+val keystoreProperties = Properties()
+
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+android {
+    signingConfigs {
+        create("signed") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
+    namespace = "pl.baftek.spitfire"
+
+    compileSdk = 33
+
+    sourceSets {
+        getByName("main") {
+            manifest.srcFile("AndroidManifest.xml")
+            java.setSrcDirs(listOf("src"))
+            aidl.setSrcDirs(listOf("src"))
+            renderscript.setSrcDirs(listOf("src"))
+            res.setSrcDirs(listOf("res"))
+            assets.setSrcDirs(listOf("assets"))
+            jniLibs.setSrcDirs(listOf("libs"))
+        }
+    }
+
+    defaultConfig {
+        applicationId = "pl.baftek.spitfire"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1 // set automatically by fastlane + GitHub Action
+        versionName = "1.3.0"
+    }
+
+    packagingOptions {
+        resources {
+            excludes.add("META-INF/robovm/ios/robovm.xml")
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("signed")
+
+        }
+    }
+}
+
+// called every time gradle gets executed, takes the native dependencies of
+// the natives configuration, and extracts them to the proper libs/ folders
+// so they get packed with the APK.
+tasks.register<Copy>("copyAndroidNatives") {
+    doFirst {
+        file("libs/armeabi-v7a/").mkdirs()
+        file("libs/arm64-v8a/").mkdirs()
+        file("libs/x86_64/").mkdirs()
+        file("libs/x86/").mkdirs()
+
+        configurations.natives.get().files.forEach { jar ->
+            var outputDir: File? = null
+            if (jar.name.endsWith("natives-arm64-v8a.jar")) outputDir = file("libs/arm64-v8a")
+            if (jar.name.endsWith("natives-armeabi-v7a.jar")) outputDir = file("libs/armeabi-v7a")
+            if (jar.name.endsWith("natives-x86_64.jar")) outputDir = file("libs/x86_64")
+            if (jar.name.endsWith("natives-x86.jar")) outputDir = file("libs/x86")
+            if (outputDir != null) {
+                copy {
+                    from(zipTree(jar))
+                    into(outputDir)
+                    include("*.so")
+                }
+            }
+        }
+    }
+}
+
+tasks.register<Exec>("run") {
+    description = "Runs the application with ADB"
+    group = "install"
+
+    val localProperties = project.file("../local.properties")
+    val path = if (localProperties.exists()) {
+        val properties = Properties()
+        properties.load(localProperties.inputStream())
+        val sdkDir = properties.getProperty("sdk.dir")
+        sdkDir ?: System.getenv("ANDROID_HOME")
+    } else {
+        System.getenv("ANDROID_HOME")
+    }
+
+    val adb = "$path/platform-tools/adb"
+    commandLine(adb, "shell", "am", "start", "-n", "pl.baftek.spitfire/pl.baftek.spitfire.AndroidLauncher")
+}
+
+/*// sets up the Android Idea project, using the old Ant based build.
+idea {
+    module {
+        sourceDirs += file("src")
+
+        iml {
+            withXml {
+                def node = it.asNode()
+                def builder = NodeBuilder.newInstance()
+                builder.current = node
+                builder.component(name: "FacetManager") {
+                    facet(type: "android", name: "Android") {
+                        configuration {
+                            option(name: "UPDATE_PROPERTY_FILES", value: "true")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}*/
+
+dependencies {}
