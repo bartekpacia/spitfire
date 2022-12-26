5080712ae537c9a91e96030555b3cfe46a39f721
Bartek Pacia <barpac02@gmail.com>
2022-12-27 00:16:59 +0100

    migrate core/build.gradle to kts

---
+++ core/build.gradle.kts
@@ -0,0 +1,12 @@
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/"))
+        }
+    }
+}
