8b636c373ce0ad76e3866dfc04a52a5138a74be9
Bartek Pacia <barpac02@gmail.com>
2022-12-27 12:59:11 +0100

    move ios project configuration to ios/build.gradle.kts (but robovm plugin doesnt work)

--- ios/build.gradle.kts
+++ ios/build.gradle.kts
@@ -1,3 +1,30 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    dependencies {
+        //classpath("com.mobidevelop.robovm:robovm-gradle-plugin:2.3.18")
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
+}
+
+plugins {
+    `java-library`
+}
+
+apply(plugin = "robovm")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 sourceSets {
     main {
         java {
@@ -6,9 +33,30 @@ sourceSets {
     }
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+dependencies {
+    val roboVMVersion: String by rootProject.extra
+    val gdxVersion: String by rootProject.extra
+    val gamesvcsVersion: String by rootProject.extra
+
+    implementation(project(":core"))
+    api("com.mobidevelop.robovm:robovm-rt:$roboVMVersion")
+    api("com.mobidevelop.robovm:robovm-cocoatouch:$roboVMVersion")
+    api("com.badlogicgames.gdx:gdx-backend-robovm:$gdxVersion")
+    api("com.badlogicgames.gdx:gdx-platform:$gdxVersion:natives-ios")
+
+    implementation("com.badlogicgames.gdx:gdx-freetype-platform:$gdxVersion:natives-ios")
+
+    implementation("de.golfgl.gdxgamesvcs:gdx-gamesvcs-ios-gamecenter:$gamesvcsVersion")
+}
+
+robovm {
+    val isAppStore = System.getenv("SPITFIRE_APP_STORE")
+    val profile = if (isAppStore == "true") "AppStore" else "AdHoc"
+
+    iosSignIdentity = "Apple Distribution: Bartek Pacia (VX8P4VC6NH)"
+    iosProvisioningProfile = "match $profile pl.baftek.spitfire"
+    isIosSkipSigning = false
+    archs = "arm64"
 }
 
 extra.apply {
