1f340e2beab22e135f8ca3a1dfcc8e71dc441d44
Bartek Pacia <barpac02@gmail.com>
2022-12-27 15:22:28 +0100

    move core project configuration to core/build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    `java-library`
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
@@ -9,4 +13,16 @@ sourceSets {
             setSrcDirs(listOf("src/"))
         }
     }
+}
+
+dependencies {
+    val gdxVersion: String by rootProject.extra
+    val gamesvcsVersion: String by rootProject.extra
+    val visuiVersion: String by rootProject.extra
+
+    implementation("com.badlogicgames.gdx:gdx:$gdxVersion")
+    api("com.badlogicgames.gdx:gdx-freetype:$gdxVersion")
+    api("com.kotcrab.vis:vis-ui:$visuiVersion")
+
+    implementation("de.golfgl.gdxgamesvcs:gdx-gamesvcs-core:$gamesvcsVersion")
 }
