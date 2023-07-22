1af5fe370554aab3a8f5cab0fe3ec2ec75a3ec5a
Bartek Pacia <barpac02@gmail.com>
2023-07-23 01:42:35 +0200

    bump libgdx to 1.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ allprojects {
     version = "1.0"
 
     extra["appName"] = "Spitfire"
-    extra["gdxVersion"] = "1.11.0"
+    extra["gdxVersion"] = "1.12.0"
     extra["roboVMVersion"] = "2.3.19"
     extra["gamesvcsVersion"] = "1.1.0"
     extra["visuiVersion"] = "1.5.1"
