795ad9db52c5314aba986b792bde88a8e155ba7a
Bartek Pacia <barpac02@gmail.com>
2023-07-23 01:47:26 +0200

    bump visui to 1.5.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,5 +7,5 @@ allprojects {
     extra["gdxVersion"] = "1.12.0"
     extra["roboVMVersion"] = "2.3.19"
     extra["gamesvcsVersion"] = "1.1.0"
-    extra["visuiVersion"] = "1.5.1"
+    extra["visuiVersion"] = "1.5.2"
 }
