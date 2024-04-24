781b6b99378952804382637334b246affadc1fab
Bartek Pacia <barpac02@gmail.com>
2024-04-24 02:06:31 +0200

    gradle update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ pluginManagement {
     }
 
     plugins {
-        id("robovm") version "2.3.20" apply false
+        id("robovm") version "2.3.21" apply false
         id("com.android.application") version "8.2.0" apply false
     }
 }
